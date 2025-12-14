31eaae0c3e563d2328900f065dc741fd8fc5eccf
M0RPH10U5 <m0rph10u5@hotmail.com>
2025-12-14 08:48:05 -0600

    update

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function renderMembers(members) {
         card.className = 'card';
         card.innerHTML = `
             <h2>${member.name}</h2>
-            <h4>SC Name:</h4><span class="sc-name"> ${member["sc-name"]}</span>
+            <h5>SC Name:</h5><span class="sc-name"> ${member["sc-name"]}</span>
             <span class="role">${member.role}</span>
             <strong>Specialty:</strong> ${member.specialty || 'N/A'}
             <br />
