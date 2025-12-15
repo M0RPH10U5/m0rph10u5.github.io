31568980a06e2d183028b932e8df6e15a62b2c55
M0RPH10U5 <m0rph10u5@hotmail.com>
2025-12-15 13:43:18 -0600

    update

--- admin/schemas/admin.schemas.js
+++ admin/schemas/admin.schemas.js
@@ -1,3 +1,6 @@
+import { LogisticsSchema } from './logistics.schema.js';
+import { LogsSchema } from './logs.schema.js';
+
 export const Schemas = {
     'polaris.json': LogisticsSchema,
     'idris.json': LogisticsSchema,
